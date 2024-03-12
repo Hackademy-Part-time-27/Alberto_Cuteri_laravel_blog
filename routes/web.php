4681732ef7ad84f75fcabdf0aac00ea459e50069
iretuc <albertocuteri88@gmail.com>
2024-03-12 21:25:01 +0100

    prima esercitazione laravel

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/chisono', function () {
     $list = ['nome','cognome','età'];
     return view ('chisono', [
         'title' => 'Ciao',
-        'subtitle' => 'mi chiamo Alberto, sono nato a Catania ed ho 35 anni',
+        'subtitle' => 'mi chiamo Alberto sono nato a Catania ed ho 35 anni',
     'list' => $list,
     ]);
 });
